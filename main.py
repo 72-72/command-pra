d9b03b12d628dedb723453add31996c9f61642e1
koyachi syusuke <s.koyachi.sys20@morijyobi.ac.jp>
2021-03-26 11:12:48 +0900

    bmi計算アプリの更新
    二回表示されるように変更

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ bmi = calc_bmi.calc_bmi(weight, height)
 
 if bmi < 18.5:
     print("あなたはやせ気味です")
+    print("あなたはやせ気味です")
 elif bmi < 24.9:
+    print("あなたは標準です")
     print("あなたは標準です")
 elif bmi < 29:
     print("あなたはやや太り気味です")
+    print("あなたはやや太り気味です")
 elif bmi >= 30:
+    print("あなたは太っています")
     print("あなたは太っています")
